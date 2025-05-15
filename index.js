f5ca0db9e4af79c3c1a80afcbae58aedb76ce10d
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-05-15 04:22:33 -0700

    conditional-statement

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+// if
+
+// else if
+
+// else
+
+// let a = 20;
+// let b = 20;
+
+// if (a > b) {
+//   console.log("a is gretter than b");
+// } else if (a < b) {
+//   console.log("a is less than b");
+// } else {
+//   console.log("a is equal to b");
+// }
+
+// let password = 9;
+
+// if (password === 8) {
+//   console.log("welcome");
+// } else if (password <= 8) {
+//   console.log("password is too short");
+// } else if (password >= 8) {
+//   console.log("password is too long");
+// } else {
+//   console.log("please provide a password");
+// }
+
